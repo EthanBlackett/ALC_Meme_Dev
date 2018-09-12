d3fadd8c937b7c53f70cd016d065a6ae1f83e291
Unknown <stockmicah@gmail.com>
2018-09-12 10:27:36 -0600

    Added CharMove
    The character can move right now

---
+++ 2D Game/Assets/Script/charMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charMove : MonoBehaviour {
+
+    //Player Movement variables
+    public int MoveSpeed;
+    public float JumpHeight;
+
+    //Player grounded variables
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	void FixedUpdate()
+	{
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//This code makes the character jump
+        if(Input.GetKeyDown (KeyCode.Space)&& grounded) {
+            Jump();
+        }
+        if(Input.GetKey (KeyCode.D)){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+	}
+    public void Jump () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,  JumpHeight);
+    }
+}
+
