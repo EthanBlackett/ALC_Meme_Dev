c06276220dbcab36f1717ffc41f978e34089493c
Unknown <stockmicah@gmail.com>
2018-09-20 10:28:29 -0600

    added deathBox, and kinda scoreManager
    they only kinda work

---
+++ 2D Game/Assets/Death.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+  
+        if(other.name == "PC")
+        {
+            Debug.Log("Player Enters Death Zone");
+            Destroy(other);
+        }
+	}
+}
