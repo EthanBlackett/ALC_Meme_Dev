db70078b9e7706cd1453358501079856abfeb68b
Unknown <stockmicah@gmail.com>
2018-10-04 10:28:34 -0600

    Changed wall collisions, progress made towards respawn working

---
+++ 2D Game/Assets/killPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killPlayer : MonoBehaviour {
+
+    public levelManager levelManager;
+
+	// Use this for initialization
+	void Start () {
+        levelManager = FindObjectOfType<levelManager>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D other) {
+        if(other.name == "PC") {
+            
+            levelManager.RespawnPlayer();
+        }
+	}
+}
