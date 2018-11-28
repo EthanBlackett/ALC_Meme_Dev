e74bfd3e2eb720abdd0f1dd32a131e08e65eda5d
Unknown <stockmicah@gmail.com>
2018-11-28 10:27:59 -0700

    Added a main menu
    it works fam

---
+++ 2D Game/Assets/Script/mainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+    public int levelToLoad;
+
+
+	// Use this for initialization
+	public void LoadLevel () {
+        SceneManager.LoadScene(levelToLoad);
+	}
+	
+	// Update is called once per frame
+	public void levelExit () {
+        Application.Quit();
+	}
+}
